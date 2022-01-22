ca72cf6a271ac66553612351cb69536e47408e18
Shin <cjs4022@gmail.com>
2022-01-22 17:17:50 +0900

    [Update] BOJ-1270 로프문제 풀이

---
+++ GwangCheon/로프.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(input())
+
+rope = [int(sys.stdin.readline()) for i in range(n)]
+
+rope.sort(reverse=True)
+
+weight = [rope[i] * (i + 1) for i in range(n)]
+print(max(weight))
+
+# for i in range(n):
+#     weight.append(rope[i] * (i+1))
+
+
